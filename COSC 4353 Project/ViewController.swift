a058efe6016a9889336a8c1699e08d06e683de34
jaredtalbert <jjtalbert@uh.edu>
2020-06-16 17:56:20 -0500

    Initial commit

---
+++ COSC 4353 Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COSC 4353 Project
+//
+//  Created by Jared Talbert on 6/16/20.
+//  Copyright © 2020 Jared Talbert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+ 
+
+}
+
